b37f6a5f86ad82dc49aead45a66ad70a1ee6fcf4
dowglasmaia <dowglasmaia@gmail.com>
2020-08-20 16:08:40 -0300

    impl progresso com @Input()

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -15,6 +15,7 @@ export class PainelComponent implements OnInit {
 
   public rodada: number = 0;
   public rodadaFrase: Frase;
+  public progresso: number = 0;
 
   constructor() {
     this.rodadaFrase = this.frases[this.rodada];
@@ -28,8 +29,6 @@ export class PainelComponent implements OnInit {
 
   public atualizarResposta(resposta: Event): void {
     this.resposta = (<HTMLInputElement>resposta.target).value;
-
-    //console.log(this.resposta)
   }
 
   public verificarResposta() {
@@ -39,7 +38,12 @@ export class PainelComponent implements OnInit {
 
       this.rodada++;
 
-      console.log(this.rodada)
+      //progresso
+      this.progresso = this.progresso + (100 / this.frases.length )
+
+      console.log(this.progresso)
+
+      this.rodadaFrase = null;
 
       this.rodadaFrase = this.frases[this.rodada];
     } else {
