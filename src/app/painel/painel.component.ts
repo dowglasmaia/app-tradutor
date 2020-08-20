6d535111c97a00d06e56393df3b63a555fbc89d4
dowglasmaia <dowglasmaia@gmail.com>
2020-08-19 23:49:41 -0300

    impl rodadas e alterando frases dinamicamente

--- src/app/painel/painel.component.ts
+++ src/app/painel/painel.component.ts
@@ -11,9 +11,15 @@ export class PainelComponent implements OnInit {
 
   public frases: Frase[] = FRASES;
   public instrucao: string = 'Traduza a frase.'
-  public resposta: string ;
+  public resposta: string;
 
-  constructor() { }
+  public rodada: number = 0;
+  public rodadaFrase: Frase;
+
+  constructor() {
+    this.rodadaFrase = this.frases[this.rodada];
+    console.log(this.rodadaFrase)
+  }
 
   ngOnInit(): void {
 
@@ -21,8 +27,24 @@ export class PainelComponent implements OnInit {
   }
 
   public atualizarResposta(resposta: Event): void {
-    this.resposta = (<HTMLInputElement>resposta.target).value ;
-    console.log(this.resposta)
+    this.resposta = (<HTMLInputElement>resposta.target).value;
+
+    //console.log(this.resposta)
+  }
+
+  public verificarResposta() {
+
+    if (this.rodadaFrase.frasePtBr == this.resposta) {
+      alert("Tradução correta")
+
+      this.rodada++;
+
+      console.log(this.rodada)
+
+      this.rodadaFrase = this.frases[this.rodada];
+    } else {
+      alert("Tradução Errada")
+    }
 
   }
 
